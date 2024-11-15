bc2cacb90489b55fe4bcdbbe3ab9c45702f4d50e
mouad elbouanani <mouadrlb@gmail.com>
2024-11-15 21:12:09 +0100

    first

---
+++ src/main/java/mouad/kiosk_back/KioskBackApplication.java
@@ -0,0 +1,13 @@
+package mouad.kiosk_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mouad/kiosk_back/KioskBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mouad.kiosk_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
